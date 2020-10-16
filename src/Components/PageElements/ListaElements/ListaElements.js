03b2b653bcb2c0acec0a7d516ef4cc740dae5b87
Diego-Nascimento <diegostealth3@gmail.com>
2020-10-16 16:57:12 -0300

    Link

--- src/Components/PageElements/ListaElements/ListaElements.js
+++ src/Components/PageElements/ListaElements/ListaElements.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Lista, Item } from './ListaElements.style';
 import Button from '../../Blue-Button/BlueButton';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 export default function ListaElements({ cars }) {
   return (
