d41ddc0f7eabc9208894911935b0fc800b9bd3f0
Diego-Nascimento <diegostealth3@gmail.com>
2020-10-16 16:59:17 -0300

    sem static routes

--- src/pages/cars/[id].js
+++ src/pages/cars/[id].js
@@ -13,23 +13,6 @@ const Carro = ({ data }) => {
 
 export default Carro;
 
-export async function getStaticPaths() {
-  const response = await api.get('cars_ids');
-  const dado = [];
-
-  response.data.forEach((element) => {
-    dado.push({
-      params: {
-        id: element.id,
-      },
-    });
-  });
-  return {
-    paths: dado,
-    fallback: false,
-  };
-}
-
 export async function getStaticProps({ params }) {
   try {
     const response = await api.get(`cars/` + params.id);
